bddbb623b9c75848b3d234b749ea46d4450853d6
Vithurshan2002 <sangervithu2002@gmail.com>
2025-05-05 22:25:47 +0530

    add

--- Exercise_1/apicallings/src/App.jsx
+++ Exercise_1/apicallings/src/App.jsx
@@ -1,13 +1,26 @@
-import { useEffect } from "react"
-
+import { useEffect, useState } from "react"
+import axios from 'axios';
 function App() {
  
+  const [datas,setdata]=useState([]);
     function ApiCall()
     {
-        
+         axios.get("https://jsonplaceholder.typicode.com/todos/1").then((response)=>{
+            setdata(()=>[...datas,response.data]);
+            console.log(datas);
+        }).catch((err)=>{
+          console.log(err);
+        }) 
+
+
+         
     }
 
-  useEffect(ApiCall(),[])
+  useEffect(()=>{
+    ApiCall();
+  },[]
+ )
+
   return (
     <>
       
