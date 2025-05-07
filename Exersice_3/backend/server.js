200ff998b7d1cd05c0f32d7faab6b4c3d956319b
Vithurshan2002 <sangervithu2002@gmail.com>
2025-05-07 15:46:27 +0530

    exersise3

---
+++ Exersice_3/backend/server.js
@@ -0,0 +1,72 @@
+const { json } = require('body-parser');
+const { error } = require('console');
+const express=require('express');
+const multer=require('multer');
+const app=express();
+const path=require('path');
+
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+            cb(null,'./uploads');
+    },
+    filename:(req,file,cb)=>{
+        console.log(file);
+         const extention=path.extname(file.originalname);
+         const newname=`${Date.now()}${extention}`;
+         cb(null,newname);
+    }       
+
+    
+})
+
+let filsize=1*1024*1024
+
+const fileFilter=(req,file,cb)=>
+{
+    const allowedtype=  /jpeg|jpg|png|pdf/;
+    const isallowed=allowedtype.test(file.minetype);
+    if(isallowed){
+        cb(null,true)
+    }
+    else{
+        cb(new Error("file type is differ"),false)
+    }
+
+}
+
+//multerconfig
+const upload=multer(
+    {
+        storage:storage,
+        limits:{
+            fileSize:filsize,
+        },
+        fileFilter:fileFilter,
+    }
+)
+
+app.post('/imageuploading',upload.array("imagedemo",5),(req,res,next)=>{
+    
+       res.json({message:"ImageUplaoded",data:req.files})
+       
+});
+app.use((error,req,res,next)=>{
+    if(error instanceof multer.MulterError){
+        switch(error.code){
+            case 'LIMIT_FILE_SIZE':
+                res.json({message:"file too large"})
+                break;
+            default:
+                res.json({message:"server error"});
+                break;
+        }
+    }
+    else{
+        res.json({message:"differnt file type"});
+    }
+});
+app.listen(3000,()=>{console.log("port listening")});
+
+
+
+
